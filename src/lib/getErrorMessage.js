a64f7cd88cb13322838fd1695b3ff645dc49bc90
weiyanqin <wanghaiming723@foxmail.com>
2018-11-08 16:21:37 +0800

    show error

--- src/lib/getErrorMessage.js
+++ src/lib/getErrorMessage.js
@@ -1,7 +1,7 @@
 const map = {
     202:  '用户名已被占用',
     217:  '无效的用户名',
-    unknown:  '注册失败，请稍后再试'
+    unknown:  '请求失败，请稍后再试'
   }
   export default function({code}){
     return map[code] || map.unknown
